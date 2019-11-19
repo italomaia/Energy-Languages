b3b69522e3fbf0f5935c859d2045f44f468ffb05
Italo Maia <italo.maia@gmail.com>
2019-11-19 21:26:16 +0100

    Adding support to lua, erlang, c, c++, perl, python and rust, with dockerfile

---
+++ PROGRESS.md
@@ -0,0 +1,14 @@
+# Progress of the project
+
+## Migrated Languages
+
+* c
+* c++
+* erlang
+* lua51
+* lua53
+* luajit
+* python2
+* python3
+* ruby
+* rust
--- gen-input.sh
+++ gen-input.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Generating input for k-nucleotide benchmark"
-/usr/bin/env python3 Python/fasta/fasta.python3-3.py 25000000 > knucleotide-input25000000.txt
+/usr/bin/env python3 python3/fasta/fasta.python3-3.py 25000000 > rapl/knucleotide-input25000000.txt
 
 echo "Generating input for reverse-complement benchmark"
-/usr/bin/env python3 Python/fasta/fasta.python3-3.py 25000000 > revcomp-input25000000.txt
+/usr/bin/env python3 python3/fasta/fasta.python3-3.py 25000000 > rapl/revcomp-input25000000.txt
 
 echo "Generating input for regex-redux benchmark"
-/usr/bin/env python3 Python/fasta/fasta.python3-3.py 5000000 > regexredux-input5000000.txt
+/usr/bin/env python3 python3/fasta/fasta.python3-3.py 5000000 > rapl/regexredux-input5000000.txt
