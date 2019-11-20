5fd39c18297a86dbfb4faa45afe1beae078da20a
Italo Maia <italo.maia@gmail.com>
2019-11-20 04:34:22 +0100

    fixed option docs

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def make_parser():
     parser.add_argument(
         '-b', '--force-build',
         default=False,
-        help='force build docker images',
+        help='build docker images even if they already exist',
         action='store_true'
     )
 
