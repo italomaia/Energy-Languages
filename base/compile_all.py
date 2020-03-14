ebf344f5c9518b31048c0062890ef37d43b9d268
Italo Maia <italo.maia@gmail.com>
2020-03-14 02:44:09 +0100

    Improved error handling

--- base/compile_all.py
+++ base/compile_all.py
@@ -2,62 +2,68 @@ import os
 import time
 import argparse
 
-from subprocess import call
-from subprocess import check_output
-from subprocess import Popen
-from subprocess import PIPE
+from subprocess import check_output  # nosec
+from subprocess import CalledProcessError  # nosec
+from subprocess import PIPE  # nosec
 
 path = '.'
 action = 'compile'
 
 
-def file_exists(file_path):
-    return os.path.isfile(file_path) if file_path \
+def file_exists(fpath: str) -> bool:
+    return os.path.isfile(fpath) if fpath \
         else False
 
 
-def main(action):
+def clean_results():
+    t_lang = os.getenv('TLANG')  # language name
+    results_filepath = f"/opt/results/{t_lang}.txt"
+
+    # clean measures before running
+    if os.path.exists(results_filepath):
+        os.remove(results_filepath)
+
+
+def clean_measures(lang):
+    t_lang = os.getenv('TLANG')  # language name
+    measures_filepath = f"/opt/measures/{t_lang}.txt"
+
+    # clean measures before running
+    if os.path.exists(measures_filepath):
+        os.remove(measures_filepath)
+
+
+def main(action: str):
     for root, dirs, files in os.walk(path):
         print('compile_all: checking ' + root)
         makefile = os.path.join(root, "Makefile")
 
         if file_exists(makefile):
             cmd = ['make', action]
-            print("compile_all: " + ' '.join(cmd))
+            print(("compile_all: " + ' '.join(cmd)).strip())
 
             if action == 'run':
-                t_lang = os.getenv('TLANG')  # language name
-                results_filepath = f"/opt/results/{t_lang}.txt"
-
-                # clean measures before running
-                if os.path.exists(results_filepath):
-                    os.remove(results_filepath)
+                clean_results()
 
             if action == 'measure':
-                t_lang = os.getenv('TLANG')  # language name
-                measures_filepath = f"/opt/measures/{t_lang}.txt"
+                clean_measures()
 
-                # clean measures before running
-                if os.path.exists(measures_filepath):
-                    os.remove(measures_filepath)
+            try:
+                msg = check_output(
+                    cmd,
+                    cwd=root,
+                    stderr=PIPE
+                ).decode('utf-8').strip()
 
-            pipes = Popen(cmd, cwd=root, bufsize=0, stdout=PIPE, stderr=PIPE)
-            std_out, std_err = pipes.communicate()
-            std_out, std_err = std_out.decode(), std_err.decode()
+                if action in ('compile', 'run'):
+                    print(f"[OK] {msg}")
+            except CalledProcessError as err:
+                out_msg = err.stdout.decode().strip()
+                err_msg = err.stderr.decode().strip()
+                err_code = err.returncode
 
-            if action in ('compile', 'run'):
-                if pipes.returncode and len(std_err):
-                    print('[OK]')
-                elif len(std_err):
-                    # return code is 0 (no error), but we may want to
-                    # do something with the info on std_err
-                    # i.e. logger.warning(std_err)
-                    print('[OK]')
-                else:
-                    # an error happened!
-                    err_msg = "%s. Code: %s" % (std_err.strip(), pipes.returncode)
-                    print('[E] Error on ' + root + ': ')
-                    print(err_msg)
+                print(f"[M] {out_msg}; Code {err_code}")
+                print(f"[E] {err_msg}; Code {err_code}")
 
         if action == 'measure':
             time.sleep(5)
