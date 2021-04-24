c546b08d2ec61ae206d614a52344f5d427791374
Italo Maia <italo.maia@gmail.com>
2021-04-24 23:13:43 +0200

    fasta file was renamed; updating reference

--- gen-input.sh
+++ gen-input.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Generating input for k-nucleotide benchmark"
-/usr/bin/env python3 python3/fasta/fasta.python3-3.py 25000000 > data/knucleotide-input25000000.txt
+/usr/bin/env python3 python3/fasta/fasta.py 25000000 > data/knucleotide-input25000000.txt
 
 echo "Generating input for reverse-complement benchmark"
-/usr/bin/env python3 python3/fasta/fasta.python3-3.py 25000000 > data/revcomp-input25000000.txt
+/usr/bin/env python3 python3/fasta/fasta.py 25000000 > data/revcomp-input25000000.txt
 
 echo "Generating input for regex-redux benchmark"
-/usr/bin/env python3 python3/fasta/fasta.python3-3.py 5000000 > data/regexredux-input5000000.txt
+/usr/bin/env python3 python3/fasta/fasta.py 5000000 > data/regexredux-input5000000.txt
